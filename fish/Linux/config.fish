3bc99228403f937e6582b04a9d61942a72ec0ef2
Th3Unknovvn <ngakobeo@gmail.com>
2025-01-12 14:02:35 +0700

    Init repo, add bash and fish config

---
+++ fish/Linux/config.fish
@@ -0,0 +1,5 @@
+set -g -x fish_greeting ''
+alias cls="clear"
+alias ...="cd ../.."
+alias ....="cd ../../.."
+clear
---
+++ fish/WSL/config.fish
@@ -0,0 +1,7 @@
+set -g -x fish_greeting ''
+alias cls="clear"
+alias np="notepad++.exe"
+alias clip="clip.exe"
+alias ...="cd ../.."
+alias ....="cd ../../.."
+clear
